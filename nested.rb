815e9130cce9950d0b6018c6b178a10280897477
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-01 23:45:35 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -66,6 +66,9 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -101,7 +104,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+:alan_kay = {known_for: "Object Orientation", alans_new_info: "GUI"}
+
+puts :alan_kay     
      
 end
 
